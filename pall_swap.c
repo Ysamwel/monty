0baffd160e9414b9a11752cd611766aae61a1787
Ysamwel <yvonneatieno2000@gmail.com>
2023-08-25 12:30:05 +0000

    monty

---
+++ pall_swap.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+/**
+ * f_pall - function that displays everything
+ * @head: double linked list head pointer to the stack
+ * @counter: unused line counter
+ * Return: Nothing
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
+/**
+ * f_swap - function that swapping of the two elements 0n the top of the stack
+ * @head: head of stack
+ * @counter: line counter
+ * Return: Nothing
+ */
+void f_swap(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->n;
+	h->n = h->next->n;
+	h->next->n = temp;
+}
